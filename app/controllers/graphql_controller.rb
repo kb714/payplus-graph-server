9e2d17d013381bad05c546f0628353681256da3e
Alexis Aguirre <alexis@acvp.cl>
2017-10-26 20:41:56 -0300

    update and refactor

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,22 +2,24 @@ class GraphqlController < ApplicationController
   # before_action :authenticate_user!
 
   def execute
-    variables = ensure_hash(params[:variables])
-    query = params[:query]
-    operation_name = params[:operationName]
-    # Query context goes here, for example:
     context = {
       current_user: User.find_by_api_key(
         request.headers['Authorization']
       )
     }
     if context[:current_user]
-      result = PayplusGraphServerSchema.execute(
-        query,
-        variables: variables,
-        context: context,
-        operation_name: operation_name
-      )
+      if params[:operations].present?
+        operations = ensure_hash(params[:operations])
+        variables = {
+            "input" => operations[:variables].
+                merge({"file" => params["variables.file"]})
+        }
+        query     = operations[:query]
+      else
+        variables = ensure_hash(params[:variables])
+        query     = params[:query]
+      end
+      result = PayplusGraphServerSchema.execute(query, variables: variables, context: context)
       render json: result
     else
       render json: { errors: [{ message: 'You need provide a valid API key' }] },
---
+++ app/graphql/types/file_type.rb
@@ -0,0 +1,13 @@
+module Types
+  FileType = GraphQL::ScalarType.define do
+    name "File"
+    description "ActionDispatch::Http::UploadedFile"
+
+    coerce_input ->(action_dispatch_uploaded_file, ctx) {
+      # graphql_controller.rb で渡した params["variables.file"] は
+      # Railsで普通の ActionDispatch::Http::UploadedFile
+      # http://api.rubyonrails.org/classes/ActionDispatch/Http/UploadedFile.html
+      action_dispatch_uploaded_file
+    }
+  end
+end
